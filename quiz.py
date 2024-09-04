9366833c577de5e8320531e74b02fb7970afa79b
aquiles021110 <aquiles@bousigues.com>
2024-09-04 15:57:57 +0200

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,61 @@
+import pgzrun
+TITLE='The random Quiz'
+WIDTH=1000
+HEIGHT=1000
+scroll_box=Rect(0,0,1000,150)
+question_box=Rect(20,175,750,150)
+timer_box=Rect(800,175,180,150)
+awnser_box_a=Rect(20,350,350,175)
+awnser_box_b=Rect(400,350,350,175)
+awnser_box_c=Rect(20,550,350,175)
+awnser_box_d=Rect(400,550,350,175)
+skip_box=Rect(800,350,180,380)
+score=0
+total_time=10
+question_file='questions.txt'
+game_over=False
+quest=[]
+total_questions=0
+current_question=0
+boxes=[awnser_box_a,awnser_box_b,awnser_box_c,awnser_box_d]
+scroller_msg=''
+def draw():
+    global scroller_msg
+    screen.clear()
+    screen.fill('Black')
+    screen.draw.filled_rect(scroll_box,'black')
+    screen.draw.filled_rect(question_box,'thistle')
+    screen.draw.filled_rect(timer_box,'darkolivegreen')
+    screen.draw.filled_rect(awnser_box_a,'firebrick')
+    screen.draw.filled_rect(awnser_box_b,'royalblue')
+    screen.draw.filled_rect(awnser_box_c,'gold')
+    screen.draw.filled_rect(awnser_box_d,'limegreen')
+    screen.draw.filled_rect(skip_box,'slategrey')
+    scroller_msg=f'Welcome to the quiz! You are on Q:{current_question}/{total_questions}'
+    screen.draw.textbox(scroller_msg,scroll_box,color='oldlace')
+    screen.draw.textbox(str(total_time),timer_box,color='goldenrod')
+    screen.draw.textbox('Skip',skip_box,color='darkorchid',angle=-90)
+    screen.draw.textbox(readqs[0],question_box,color='black')
+    index=1
+    for i in boxes:
+        screen.draw.textbox(readqs[index],i,color='black')
+        index+=1
+def scrolling():
+    scroll_box.x-=2
+    if scroll_box.right<0:
+        scroll_box.left=WIDTH
+def update():
+    scrolling()
+def text():
+    global total_questions
+    with open('questions.txt','r')as f:
+        for i in f:
+            quest.append(i)
+            total_questions+=1
+def read():
+    global current_question
+    current_question+=1
+    return quest.pop(0).split('|')
+text()
+readqs=read()
+pgzrun.go()
